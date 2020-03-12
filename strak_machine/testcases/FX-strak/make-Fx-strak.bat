95a6d7417a5c3d88f89512d394c389f044675bcc
jxjo <58261241+jxjo@users.noreply.github.com>
2020-03-12 06:24:49 +0100

    Testcases for strak machine

---
+++ strak_machine/testcases/FX-strak/make-Fx-strak.bat
@@ -0,0 +1,3 @@
+xoptfoil-jx -i iFX-strak.txt -o FX-strak-13 -r 130000 -a JX-FX-15-2a.dat
+xoptfoil-jx -i iFX-strak.txt -o FX-strak-11 -r 110000 -a FX-strak-13.dat
+xoptfoil-jx -i iFX-strak.txt -o FX-strak-09 -r  90500 -a FX-strak-11.dat
---
+++ strak_machine/testcases/SA7036-Strak/make-SA-strak.bat
@@ -0,0 +1,4 @@
+rem xoptfoil-jx -i iSA7036-strak.txt -o SA7036-strak-13 -r 130000 -a SA7036i.dat
+rem xoptfoil-jx -i iSA7036-strak.txt -o SA7036-strak-11 -r 110000 -a SA7036-strak-13.dat
+rem xoptfoil-jx -i iSA7036-strak.txt -o SA7036-strak-09 -r  90000 -a SA7036-strak-11.dat
+xoptfoil-jx -i iSA7036-tip.txt   -o SA7036-strak-07 -r  65000 -a SA7036-strak-09.dat
