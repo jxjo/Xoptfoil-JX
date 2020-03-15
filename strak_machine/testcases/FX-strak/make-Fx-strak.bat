5dde51715225df29f1ec79e14730c3807566d8dd
jxjo <58261241+jxjo@users.noreply.github.com>
2020-03-15 05:03:06 +0100

    Testcase improved

--- strak_machine/testcases/FX-strak/make-Fx-strak.bat
+++ strak_machine/testcases/FX-strak/make-Fx-strak.bat
@@ -1,3 +1,4 @@
-rem xoptfoil-jx -i iFX-strak.txt -o FX-strak-13 -r 130000 -a JX-FXrcn-15.dat
-xoptfoil-jx -i iFX-strak.txt -o FX-strak-11 -r 110000 -a FX-strak-13.dat
-xoptfoil-jx -i iFX-strak.txt -o FX-strak-09 -r  90500 -a FX-strak-11.dat
+xoptfoil-jx -i iFX-strak.txt -r 130000 -a JX-FXrcn-15.dat -o FX-strak-13 
+xoptfoil-jx -i iFX-strak.txt -r 110000 -a FX-strak-13.dat -o FX-strak-11 
+xoptfoil-jx -i iFX-strak.txt -r  90000 -a FX-strak-11.dat -o FX-strak-09 
+xoptfoil-jx -i iFX-tip.txt   -r  60000 -a FX-strak-09.dat -o FX-strak-06 
