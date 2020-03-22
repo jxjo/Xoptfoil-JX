2a6258bc51edf18bf22f04e081a849f7e957344b
Matthias231 <60029799+Matthias231@users.noreply.github.com>
2020-03-22 19:42:27 +0100

    Adaption of input-Files for strak generation
    - Modification of Batch file to rescue temporary files optimization_history and partice-files for analysis.
    - Added result-files in zipped archive.

--- strak_machine/testcases/FX-strak_WIP/make-Fx-strak.bat
+++ strak_machine/testcases/FX-strak_WIP/make-Fx-strak.bat
@@ -1,4 +1,23 @@
-xoptfoil-jx -i iFX-strak.txt -r 130000 -a JX-FXrcn-15.dat -o FX-strak-13 
+xoptfoil-jx -i iFX-strak.txt -r 130000 -a JX-FXrcn-15.dat -o FX-strak-13
+del FX-strak-13_optimization_history.dat /s /q 
+del FX-strak-13_particles.csv  /s /q 
+ren optimization_history.dat FX-strak-13_optimization_history.dat
+ren particles.csv FX-strak-13_particles.csv 
+
 xoptfoil-jx -i iFX-strak.txt -r 110000 -a FX-strak-13.dat -o FX-strak-11 
+del FX-strak-11_optimization_history.dat /s /q 
+del FX-strak-11_particles.csv  /s /q 
+ren optimization_history.dat FX-strak-11_optimization_history.dat
+ren particles.csv FX-strak-11_particles.csv 
+
 xoptfoil-jx -i iFX-strak.txt -r  90000 -a FX-strak-11.dat -o FX-strak-09 
+del FX-strak-09_optimization_history.dat /s /q 
+del FX-strak-09_particles.csv  /s /q 
+ren optimization_history.dat FX-strak-09_optimization_history.dat
+ren particles.csv FX-strak-09_particles.csv 
+
 xoptfoil-jx -i iFX-tip.txt   -r  60000 -a FX-strak-09.dat -o FX-strak-06 
+del FX-strak-06_optimization_history.dat /s /q 
+del FX-strak-06_particles.csv  /s /q 
+ren optimization_history.dat FX-strak-06_optimization_history.dat
+ren particles.csv FX-strak-06_particles.csv 
