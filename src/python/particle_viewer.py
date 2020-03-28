a250db840dae5afd7c12b12c093bacbfe8618c10
Matthias231 <60029799+Matthias231@users.noreply.github.com>
2020-03-28 12:09:10 +0100

    Update particle_viewer.py
    Added argument-parser, refactoring

--- src/python/particle_viewer.py
+++ src/python/particle_viewer.py
@@ -1,24 +1,52 @@
 #!/usr/bin/env python
 
-#  This file ...
+#  This file is part of XOPTFOIL-JX.
+
+#  XOPTFOIL-JX is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+
+#  XOPTFOIL-JX is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
 
 #  You should have received a copy of the GNU General Public License
-#  along with XOPTFOIL.  If not, see <http://www.gnu.org/licenses/>.
+#  along with XOPTFOIL-JX.  If not, see <http://www.gnu.org/licenses/>.
 
 #  Copyright (C) 2020 Matthias Boese
 
 import csv
 from matplotlib import pyplot as plt
 import numpy as np
+import argparse
+from sys import version_info
 
 ################################################################################
 
+################################################################################
+# Input function that checks python version
+def my_input(message):
 
-# Main program
-if __name__ == "__main__":
+  # Check python version
 
-     # open file containing particle-information position and velocity
-    file = open("particles.csv", "r")
+  python_version = version_info[0]
+
+  # Issue correct input command
+
+  if (python_version == 2):
+    return raw_input(message)
+  else:
+    return input(message)
+
+
+################################################################################
+# function that reads particle-data from file
+def read_particleFile(fileName):
+    # open file containing particle-information position and velocity
+    print ("Reading file %s.." % fileName)
+    file = open(fileName, "r")
 
     # create empty list
     AllIterations = []
@@ -57,20 +85,28 @@ if __name__ == "__main__":
         # Append Iteration, containig all particles to List of all Iterations
         AllIterations.append(Iteration)
     file.close()
+    print ("success.")
+
+    return AllIterations
+
+
+################################################################################
+# function that plots particle-data
+def plot_particleData(data):
 
     # always start with iteration number 1
     iterationNum = 1
     iterationNumbers = []
 
     # determine number of particles
-    firstIteration = AllIterations[0]
+    firstIteration = data[0]
     NumParticles = len(firstIteration)
-    print ("Number of particles: %d\n" % NumParticles)
+    print ("Number of particles: %d" % NumParticles)
 
     # determine number of dimensions
     firstParticle = firstIteration[0]
     NumDimensions = len(firstParticle["pos"])
-    print ("Number of dimensions: %d\n" % NumDimensions)
+    print ("Number of dimensions: %d" % NumDimensions)
 
     # create multi-dimensional, empty array
     posValues = []
@@ -88,7 +124,7 @@ if __name__ == "__main__":
             posValues[i-1].append([])
             velValues[i-1].append([])
 
-    for iteration in AllIterations:
+    for iteration in data:
         particleNum = 0
 
         for particle in iteration:
@@ -142,5 +178,41 @@ if __name__ == "__main__":
         plt.grid(True)
 
     plt.show()
+
+
+################################################################################
+# Main particle_visualizer program
+if __name__ == "__main__":
+
+# initiate the parser
+    parser = argparse.ArgumentParser('')
+    parser.add_argument("--input", "-i", help="filename containing particle-data (e.g. particles)")
+
+  # read arguments from the command line
+    args = parser.parse_args()
+
+    if args.input:
+        fileName = args.input
+    else:
+        message = "Enter the filename of the particle-file"\
+        "(e.g., particles, which  is the default filename), "\
+        "hit <enter> for default:"
+
+        fileName = my_input(message)
+
+        # set default filename in case there was no input
+        if (fileName == ""):
+            fileName = 'particles'
+
+    fileName = fileName + '.csv'
+    print("filename is %s" % fileName)
+
+    try:
+        particleData = read_particleFile(fileName)
+    except:
+        print("Error, file %s could not be opened." % fileName)
+        exit -1
+
+    plot_particleData(particleData)
 
     print("Ready.")
