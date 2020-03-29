219074c0e4a59d18bde3828b1c3bbec897e8c28d
Matthias231 <60029799+Matthias231@users.noreply.github.com>
2020-03-29 19:35:12 +0200

    improvement of planformdata.txt
    better readable
    start planform_creator.py asyncronous and wait 3 s

--- strak_machine/testcases/Fx-wing/make_fx_wing.bat
+++ strak_machine/testcases/Fx-wing/make_fx_wing.bat
@@ -1,5 +1,6 @@
 rem apply eliptic wing-planform to existing XFLR5-XML-File
-python ..\..\..\src\python\planform_creator.py
+start "" python ..\..\..\src\python\planform_creator.py
+timeout /T 3
 
 rem create batchfile for strak generation
 python ..\..\..\src\python\strak_machine.py -x plane_out -o make_fx_strak -i iFX-strak -it iFX-tip -r 150000
