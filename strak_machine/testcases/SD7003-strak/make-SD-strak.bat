8a1b05783805fd583374669f9d16c068525c17e4
jxjo <58261241+jxjo@users.noreply.github.com>
2020-03-31 16:44:56 +0200

    New demo case SD7003 strak
    similar to the MHSD strak

---
+++ strak_machine/testcases/SD7003-strak/make-SD-strak.bat
@@ -0,0 +1,6 @@
+xoptfoil-jx -i iSD-root.txt  -r 220000 -a SD7003.dat         -o SD-root-22 
+rem xoptfoil-jx -i iSD-strak.txt -r 190000 -a SD-root-22.dat    -o SD-strak-19 
+rem xoptfoil-jx -i iSD-strak.txt -r 160000 -a SD-root-22.dat    -o SD-strak-16 
+rem xoptfoil-jx -i iSD-strak.txt -r 130000 -a SD-root-22.dat    -o SD-strak-13 
+rem xoptfoil-jx -i iSD-strak.txt -r 100000 -a SD-root-22.dat    -o SD-strak-10 
+rem xoptfoil-jx -i iSD-strak.txt -r  70500 -a SD-root-22.dat    -o SD-strak-07 
