da3eec66ee28e4eeba8d2770eacdb28fbe7e8625
jxjo <58261241+jxjo@users.noreply.github.com>
2020-04-05 10:12:03 +0200

    Testcase SA7036 strak updated

--- strak_machine/testcases/SA7036-Strak/make-SA-strak.bat
+++ strak_machine/testcases/SA7036-Strak/make-SA-strak.bat
@@ -1,4 +1,4 @@
-rem xoptfoil-jx -i iSA7036-strak.txt -o SA7036-strak-13 -r 130000 -a SA7036i.dat
-rem xoptfoil-jx -i iSA7036-strak.txt -o SA7036-strak-11 -r 110000 -a SA7036-strak-13.dat
-rem xoptfoil-jx -i iSA7036-strak.txt -o SA7036-strak-09 -r  90000 -a SA7036-strak-11.dat
-xoptfoil-jx -i iSA7036-tip.txt   -o SA7036-strak-07 -r  65000 -a SA7036-strak-09.dat
+xoptfoil-jx -i iSA-strak.txt -r 130000 -a SA7036i.dat	-o SA-strak-13
+xoptfoil-jx -i iSA-strak.txt -r 110000 -a SA7036i.dat	-o SA-strak-11 
+xoptfoil-jx -i iSA-strak.txt -r  90000 -a SA7036i.dat   -o SA-strak-09 
+xoptfoil-jx -i iSA-tip.txt   -r  65000 -a SA7036i.dat   -o SA-strak-07 
