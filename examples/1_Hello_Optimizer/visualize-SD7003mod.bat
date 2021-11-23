7e4cac856011c9a7b415190d57b2c560f0cdb4e4
jxjo <58261241+jxjo@users.noreply.github.com>
2021-11-23 06:38:22 +0100

    Updated "Hello World" example

--- examples/1_Hello_Optimizer/visualize-SD7003mod.bat
+++ examples/1_Hello_Optimizer/visualize-SD7003mod.bat
@@ -5,6 +5,7 @@ rem Is there a Visualizer exe in the default directory?
 if exist ..\..\windows\bin\xoptfoil_visualizer-jx.exe (
   ..\..\windows\bin\xoptfoil_visualizer-jx -c %Airfoil% -o 3
 ) else (
-rem Use normal python search path
+rem Use normal python search path for the python script
   xoptfoil_visualizer-jx.py -c %Airfoil% -o 3
+  pause
 )
