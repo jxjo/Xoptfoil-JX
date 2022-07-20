022900ed7f195b9244682100017ef8f92a60137e
Matthias231 <60029799+Matthias231@users.noreply.github.com>
2022-07-20 08:07:36 +0200

    strak machine: removing unnecessary files
     also added SD7003 in airfoil library

--- strak_machine/testcases/FX-strak/make-Fx-strak.bat
+++
@@ -1,4 +0,0 @@
-xoptfoil-jx -i iFX-strak.txt -r 130000 -a JX-FXrcn-15.dat -o FX-strak-13 
-xoptfoil-jx -i iFX-strak.txt -r 110000 -a FX-strak-13.dat -o FX-strak-11 
-xoptfoil-jx -i iFX-strak.txt -r  90000 -a FX-strak-11.dat -o FX-strak-09 
-xoptfoil-jx -i iFX-tip.txt   -r  60000 -a FX-strak-09.dat -o FX-strak-06 
--- strak_machine/testcases/SA7036-Strak/make-SA-strak.bat
+++
@@ -1,4 +0,0 @@
-xoptfoil-jx -i iSA-strak.txt -r 130000 -a SA7036i.dat	-o SA-strak-13
-xoptfoil-jx -i iSA-strak.txt -r 110000 -a SA7036i.dat	-o SA-strak-11 
-xoptfoil-jx -i iSA-strak.txt -r  90000 -a SA7036i.dat   -o SA-strak-09 
-xoptfoil-jx -i iSA-tip.txt   -r  65000 -a SA7036i.dat   -o SA-strak-07 
